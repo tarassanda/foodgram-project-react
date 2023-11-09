82d9be0d0bb91c52072ec6c591294cd0624c983b
tarassanda <tarassanda@gmail.com>
2023-11-09 18:07:26 +0300

    First action try

--- backend/foodgram/API/serializers.py
+++ backend/foodgram/API/serializers.py
@@ -16,7 +16,6 @@ class Base64ImageField(serializers.ImageField):
         if isinstance(data, str) and data.startswith('data:image'):
             format, imgstr = data.split(';base64,')
             ext = format.split('/')[-1]
-
             data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
 
         return super().to_internal_value(data)
