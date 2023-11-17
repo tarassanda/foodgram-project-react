168d4d8c05dd2469803f452413751205b51afb2c
tarassanda <tarassanda@gmail.com>
2023-11-17 20:03:17 +0300

    Fix isort

--- backend/foodgram/recipes/migrations/0001_initial.py
+++ backend/foodgram/recipes/migrations/0001_initial.py
@@ -5,10 +5,9 @@ import django.contrib.auth.models
 import django.core.validators
 import django.db.models.deletion
 import django.utils.timezone
+import recipes.validators
 from django.conf import settings
 from django.db import migrations, models
-
-import recipes.validators
 
 
 class Migration(migrations.Migration):
